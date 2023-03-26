40b38f40d7faaccaff678ce23b810b971af264c2
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-26 10:22:43 +0500

    Add project files.

---
+++ 7.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 0;
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out num))
+            {
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+        }
+
+        int factorial = 1;
+
+        for (int i = 2; i <= num; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine("Factorial of {0} is: {1}.", num, factorial);
+    }
+}
